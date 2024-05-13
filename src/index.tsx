6a55395bdc149e8327be8cecdd25968b47bc35c7
Sagiv Mishaan <sagiv505@gmail.com>
2024-05-13 12:29:54 +0300

    Added general app provider for user preferences and inputs

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client"
 import BaliBot from "./components/BaliBot/BaliBot.tsx"
 import "./index.css"
 import BinanceProvider from "./providers/BinanceProvider.tsx"
+import GeneralProvider from "./providers/GeneralProvider.tsx"
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BinanceProvider>
-      <BaliBot />
-    </BinanceProvider>
+    <GeneralProvider>
+      <BinanceProvider>
+        <BaliBot />
+      </BinanceProvider>
+    </GeneralProvider>
   </React.StrictMode>
 )
---
+++ src/providers/GeneralProvider.tsx
@@ -0,0 +1,94 @@
+import {
+  Dispatch,
+  SetStateAction,
+  createContext,
+  useCallback,
+  useContext,
+  useMemo,
+  useState
+} from "react"
+import { ProviderProps, WatchlistItemType } from "../types"
+import { MultiValue } from "react-select"
+
+interface IGeneralContext {
+  watchlist: WatchlistItemType[]
+  tempWatchlist: WatchlistItemType[]
+  setWatchlist: Dispatch<SetStateAction<WatchlistItemType[]>>
+  setTempWatchlist: Dispatch<SetStateAction<WatchlistItemType[]>>
+  handleAddNewWatchlistItems: () => void
+  handleAddTempWatchlist: (
+    values: MultiValue<{
+      value: string
+      label: string
+    } | null>
+  ) => void
+}
+
+const defaultGeneralContext: IGeneralContext = {
+  handleAddNewWatchlistItems: () => {},
+  handleAddTempWatchlist: () => {},
+  setTempWatchlist: () => [],
+  setWatchlist: () => [],
+  tempWatchlist: [],
+  watchlist: []
+}
+
+const GeneralContext = createContext<IGeneralContext>(defaultGeneralContext)
+
+export const GeneralProvider: React.FC<ProviderProps> = ({ children }) => {
+  const [watchlist, setWatchlist] = useState<WatchlistItemType[]>([])
+  const [tempWatchlist, setTempWatchlist] = useState<WatchlistItemType[]>([])
+  const [addedContract, setAddedContracts] = useState<WatchlistItemType[]>([])
+
+  const handleAddNewWatchlistItems = useCallback(() => {
+    const contracts = watchlist.map((item) => item.symbol)
+    const additionContracts = tempWatchlist.filter((item) => {
+      if (!contracts.includes(item.symbol)) {
+        return true
+      }
+    })
+    setWatchlist((prevWatchlist) => [...prevWatchlist, ...additionContracts])
+    setAddedContracts(additionContracts)
+  }, [tempWatchlist, watchlist])
+
+  const getAndRefreshAddedContracts = () => {
+    const added = addedContract
+    setAddedContracts([])
+    return added
+  }
+  const handleAddTempWatchlist = (
+    values: MultiValue<{
+      value: string
+      label: string
+    } | null>
+  ) => {
+    if (Array.isArray(values)) {
+      setTempWatchlist(
+        values.map((value) => {
+          return { symbol: value.value }
+        })
+      )
+    }
+  }
+
+  const providerMemo = useMemo<IGeneralContext>(() => {
+    return {
+      handleAddNewWatchlistItems,
+      handleAddTempWatchlist,
+      setTempWatchlist,
+      setWatchlist,
+      tempWatchlist,
+      watchlist
+    }
+  }, [handleAddNewWatchlistItems, tempWatchlist, watchlist])
+
+  return (
+    <GeneralContext.Provider value={providerMemo}>
+      {children}
+    </GeneralContext.Provider>
+  )
+}
+
+export const useGeneralContext = () => useContext(GeneralContext)
+
+export default GeneralProvider
