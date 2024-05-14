0261528bf366d61111abb8d5730f657d338b2433
Sagiv Mishaan <sagiv505@gmail.com>
2024-05-14 10:38:55 +0300

    Implemented ControlPanel (v0.1)

---
+++ src/components/ControlPanel/ControlPanel.tsx
@@ -0,0 +1,62 @@
+import { ToggleButton, ToggleButtonGroup } from "@mui/material"
+import React from "react"
+import RssFeedIcon from "@mui/icons-material/RssFeed"
+import QuestionMarkIcon from "@mui/icons-material/QuestionMark"
+import { useEffect, useState } from "react"
+import { useBinanceContext } from "../../providers/BinanceProvider"
+import "./control-panel.css"
+const defaultToggles = ["live-feed"]
+
+const ControlPanel = () => {
+  const { setIsLiveFeed, isLiveFeed } = useBinanceContext()
+  const [toggles, setToggles] = useState<string[]>(
+    isLiveFeed ? defaultToggles : []
+  )
+
+  const handleControls = (
+    event: React.MouseEvent<HTMLElement>,
+    newToggles: string[]
+  ) => {
+    console.log(newToggles)
+    setToggles(newToggles)
+  }
+
+  useEffect(() => {
+    setIsLiveFeed(toggles.includes("live-feed"))
+  }, [setIsLiveFeed, toggles])
+  return (
+    <div className='user-control-container'>
+      <ToggleButtonGroup
+        className='control-panel'
+        value={toggles}
+        onChange={handleControls}
+      >
+        <ToggleButton
+          value='live-feed'
+          className='toggle-btn'
+          classes={{ selected: "selected" }}
+        >
+          <RssFeedIcon className='live-feed-icon icon' />
+        </ToggleButton>
+        <ToggleButton
+          value='question'
+          className='toggle-btn'
+          classes={{ selected: "selected" }}
+          disabled
+        >
+          <QuestionMarkIcon className='question-icon icon' />
+        </ToggleButton>
+        <ToggleButton
+          value='question2'
+          className='toggle-btn'
+          classes={{ selected: "selected" }}
+          disabled
+        >
+          <QuestionMarkIcon className='question-icon icon' />
+        </ToggleButton>
+      </ToggleButtonGroup>
+    </div>
+  )
+}
+
+export default ControlPanel
